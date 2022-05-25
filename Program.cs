b983edfe54b8de0acaff94788702509ec1718357
MuthuKumaran Shanmugam <Muthukumaran.S01@mphasis.com>
2022-05-25 06:16:08 +0530

    Helloworld

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
